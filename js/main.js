79a8bc4098599253577d597aa06e1222bc267b51
Alex Kozenko <kozenko.cfif@gmail.com>
2015-08-29 09:53:15 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import Backbone from 'backbone';
+import $ from 'jquery';
+
+import 'styles/main.css';
+
+Backbone.history.start();
+
+console.log(43000003, $);
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+
+module.exports = {
+  entry: {
+    app: [
+      './js/main.js'
+    ]
+  },
+
+  output: {
+    path: __dirname + '/assets',
+
+    filename: 'bundle.js',
+
+    publicPath: '/assets/'
+  },
+
+  resolve: {
+    root: __dirname,
+
+    modulesDirectories: ['js', 'views', 'node_modules']
+
+  },
+
+  plugins: [
+  ],
+
+  module: {
+    // preLoaders: [
+    //   {test: /\.js$/, loader: 'eslint', exclude: /node_modules/},
+    // ],
+    loaders: [
+      {test: /\.js$/, loaders: ['babel'], exclude: /node_modules/},
+      {test: /\.css$/, loaders: ['style', 'css', 'autoprefixer']}
+    ]
+  },
+
+  devtool: '#inline-source-map',
+
+  eslint: {
+    emitErrors: true,
+    reporter: function(results) {
+      return results.map(function(result) {
+        return result.messages.map(function(msg) {
+          return (
+            ' ' + msg.message + '(' + msg.ruleId + ')' +
+            ' @ line ' + msg.line + ' column ' + msg.column +
+            ' - ' +
+            (msg.fatal ? 'fatal, ' : '') +
+            'severity: ' + msg.severity
+          );
+        }).join('\n');
+      }).join('\n');
+    }
+  }
+};
